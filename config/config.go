899417b4b9195164f73f09da2c44be1b6f32ae1a
Eddie Knight <knight@linux.com>
2023-02-08 16:46:32 -0600

    Extracted more logic from the wireframe to the raidengine package

--- config/config.go
+++ config/config.go
@@ -126,7 +126,6 @@ func (ctx *GlobalOpts) PrepareOutputDirectory(subdirectories ...string) {
 	}
 }
 
-// TODO: fix that this is defaulting to creating a new directory when using -h
 // Create directory if possible, otherwise log error to console
 func ensureDirReadiness(directory string) {
 	err := os.MkdirAll(directory, 0755)
--- raidengine/raidengine.go
+++ raidengine/raidengine.go
@@ -1,13 +1,19 @@
 package raidengine
 
 import (
+	"fmt"
 	"log"
 	"reflect"
 	"runtime"
+	"strings"
+
+	"github.com/privateerproj/privateer-sdk/config"
+	"github.com/privateerproj/privateer-sdk/utils"
 )
 
 type Strike func() error
 
+// Run is used to execute a list of strikes, intended to be pre-parsed by UniqueAttacks
 func Run(strikes []Strike) (errors []error) {
 	for _, strike := range strikes {
 		err := execStrike(strike)
@@ -15,11 +21,12 @@ func Run(strikes []Strike) (errors []error) {
 			errors = append(errors, err)
 		}
 	}
+	writeRaidLog(errors)
 	return
 }
 
 func execStrike(strike Strike) error {
-	log.Printf("Initiating Strike: %v", GetFunctionAddress(strike))
+	log.Printf("Initiating Strike: %v", getFunctionAddress(strike))
 	err := strike()
 	if err != nil {
 		log.Print(err)
@@ -27,6 +34,56 @@ func execStrike(strike Strike) error {
 	return err
 }
 
-func GetFunctionAddress(i Strike) string {
+func writeRaidLog(errors []error) {
+	config.GlobalConfig.PrepareOutputDirectory()
+	// TODO: Get user feedback on desired output
+	// for i, err := range errors {
+	// 	log.Printf("%v: %v", i, err)
+	// }
+}
+
+func GetUniqueStrikes(strikePacks map[string][]Strike, policies ...string) (strikes []Strike) {
+	log.Printf("Policies Requested: %s", strings.Join(policies, ","))
+
+	if len(policies) == 1 {
+		// If set via environment variables, this value may come in as a comma delineated string
+		policies = strings.Split(policies[0], ",")
+	}
+	for _, strike := range policies {
+		if _, ok := strikePacks[strike]; !ok {
+			log.Print(utils.ReformatError("Strike pack not found for policy: %s (Skipping)", strike))
+			continue
+		}
+		strikes = append(strikes, strikePacks[strike]...)
+	}
+	return uniqueStrikes(strikes)
+}
+
+func uniqueStrikes(allStrikes []Strike) (strikes []Strike) {
+	used := make(map[string]bool)
+	for _, strike := range allStrikes {
+		name := getFunctionAddress(strike)
+		if _, ok := used[name]; !ok {
+			used[name] = true
+			strikes = append(strikes, strike)
+		}
+	}
+	return
+}
+
+func getFunctionAddress(i Strike) string {
 	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+func GetOutput(raids []Strike, errors []error) error {
+	raidsRequested := len(raids)
+	raidsSucceeded := len(raids) - len(errors)
+	output := fmt.Sprintf("%v/%v attacks succeeded. View the output logs for more details.", raidsSucceeded, raidsRequested)
+
+	if errors != nil {
+		return utils.ReformatError(output)
+	}
+
+	log.Print(output)
+	return nil
 }
