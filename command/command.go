6a9cf2fb9d849eb2dbaa7d0b2b2e924de76f3649
Eddie Knight <knight@linux.com>
2023-02-25 14:43:44 -0600

    Fixed bug in flag handling

--- command/command.go
+++ command/command.go
@@ -28,7 +28,9 @@ func SetBase(cmd *cobra.Command) {
 	viper.BindPFlag("binaries-path", cmd.PersistentFlags().Lookup("binaries-path"))
 
 	cmd.PersistentFlags().BoolP("help", "h", false, fmt.Sprintf("Give me a heading! Help for the specified command"))
+}
 
+func InitializeConfig(cmd *cobra.Command, args []string) {
 	logger := logging.GetLogger("setup", "error", false)
 
 	viper.SetConfigFile(viper.GetString("config"))
@@ -43,8 +45,11 @@ func SetBase(cmd *cobra.Command) {
 		loglevel = "off"
 	}
 	viper.Set("loglevel", loglevel)
-
+	logger.Error(fmt.Sprintf("verbose: %v", viper.GetBool("verbose")))
 	logger = logging.GetLogger("setup", loglevel, false)
+
+	logger.Error(fmt.Sprintf("Config file flag: %s (loglevel: %s)", viper.GetString("config"), viper.GetString("loglevel")))
+	logger.Error(fmt.Sprintf("Using config file: %s (loglevel: %s)", viper.ConfigFileUsed(), viper.GetString("loglevel")))
 
 	if err := viper.ReadInConfig(); err != nil {
 		if strings.HasSuffix(err.Error(), "no such file or directory") {
