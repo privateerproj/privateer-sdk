15ffe2270affc56c30549f7e1a73ffa5b1ce6712
Eddie Knight <knight@linux.com>
2024-11-25 06:19:18 -0600

    fixed expected errors on tests
    Signed-off-by: Eddie Knight <knight@linux.com>

--- config/config_test.go
+++ config/config_test.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 
 	"github.com/spf13/viper"
@@ -19,6 +18,7 @@ var testConfigs = []struct {
 	logLevelSet    bool
 	invasiveSet    bool
 	writeDirSet    bool
+	expectedError  string
 }{
 	{
 		testName:       "Good - One Service",
@@ -52,7 +52,6 @@ services:
   my-service-1:
     tactics:
       - tlp_green
-      - tlp_clear
 `}, {
 		testName:       "Good - Log Level Set in Service",
 		runningService: "my-service-1",
@@ -64,7 +63,6 @@ services:
     loglevel: debug
     tactics:
       - tlp_green
-      - tlp_clear
 `}, {
 		testName:       "Good - Invasive Set at Top Level",
 		runningService: "my-service-1",
@@ -76,7 +74,17 @@ services:
   my-service-1:
     tactics:
       - tlp_green
-      - tlp_clear
+`}, {
+		testName:       "Good - Invasive Set at Service Level",
+		runningService: "my-service-1",
+		requiredVars:   []string{},
+		invasiveSet:    true,
+		config: `
+services:
+  my-service-1:
+    invasive: true
+    tactics:
+      - tlp_green
 `}, {
 		testName:       "Good - Write Directory Set",
 		runningService: "my-service-1",
@@ -117,6 +125,7 @@ services:
 		runningService: "my-service-1",
 		requiredVars:   []string{"missing"},
 		missingVars:    []string{"missing"},
+		expectedError:  "missing required variables: [missing]",
 		config: `
 services:
   my-service-1:
@@ -130,6 +139,7 @@ services:
 		runningService: "my-service-1",
 		requiredVars:   []string{"key", "missing"},
 		missingVars:    []string{"missing"},
+		expectedError:  "missing required variables: [missing]",
 		config: `
 services:
   my-service-1:
@@ -143,6 +153,7 @@ services:
 		runningService: "my-service-1",
 		requiredVars:   []string{"missing1", "missing2"},
 		missingVars:    []string{"missing1", "missing2"},
+		expectedError:  "missing required variables: [missing1 missing2]",
 		config: `
 services:
   my-service-1:
@@ -156,12 +167,23 @@ services:
 		runningService: "my-service-1",
 		requiredVars:   []string{"key", "missing1", "missing2"},
 		missingVars:    []string{"missing1", "missing2"},
+		expectedError:  "missing required variables: [missing1 missing2]",
 		config: `
 services:
   my-service-1:
     tactics:
       - tlp_green
       - tlp_clear
+    vars:
+      key: value
+`}, {
+		testName:       "Bad - Missing Tactics",
+		runningService: "my-service-1",
+		requiredVars:   []string{},
+		expectedError:  "no tactics specified for service my-service-1",
+		config: `
+services:
+  my-service-1:
     vars:
       key: value
 `},
@@ -178,20 +200,20 @@ func TestNewConfig(t *testing.T) {
 				t.Fatalf("error reading config: %v", err)
 			}
 
-			// create the config object
 			viper.Set("service", tt.runningService)
 			config := NewConfig(tt.requiredVars)
-			if config.Error != nil {
-				if len(tt.missingVars) > 0 {
-					expectedError := fmt.Sprintf("missing required variables: %v", tt.missingVars)
-					if config.Error.Error() != expectedError {
-						t.Errorf("expected error to be '%s', got %v", expectedError, config.Error)
-					}
-				} else {
-					t.Errorf("unexpected error: %v", config.Error)
-				}
-			} else if len(tt.missingVars) > 0 {
-				t.Errorf("expected error for missing vars, got nil")
+
+			if config.Error == nil && tt.expectedError != "" {
+				t.Errorf("expected error '%s', got nil", tt.expectedError)
+				return
+			} else if config.Error != nil && tt.expectedError == "" {
+				t.Errorf("expected no error, got %v", config.Error)
+				return
+			} else if config.Error != nil && tt.expectedError != config.Error.Error() {
+				t.Errorf("expected error '%s', got '%s'", tt.expectedError, config.Error.Error())
+				return
+			} else if config.Error != nil && tt.expectedError == config.Error.Error() {
+				return
 			}
 
 			if config.ServiceName != tt.runningService {
@@ -201,6 +223,9 @@ func TestNewConfig(t *testing.T) {
 				t.Errorf("expected log level to be different from default, but got '%s'", config.LogLevel)
 			} else if !tt.logLevelSet && config.LogLevel != "Error" {
 				t.Errorf("expected log level to be set to default, but got '%s'", config.LogLevel)
+			}
+			if tt.invasiveSet != config.Invasive {
+				t.Errorf("expected invasive to be '%v', but got '%v'", tt.invasiveSet, config.Invasive)
 			}
 			if tt.writeDirSet && config.WriteDirectory == "" {
 				t.Errorf("expected write directory to be set")
