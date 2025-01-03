790e83fdde35b246276ea87d5b8f9b9e3682d69b
Eddie Knight <knight@linux.com>
2025-01-03 12:07:31 -0600

    feat: new pass and fail fields for TestResult (#42)
    * feat: new pass and fail fields for TestResult
    
    Signed-off-by: Eddie Knight <knight@linux.com>

--- pluginkit/test.go
+++ pluginkit/test.go
@@ -11,3 +11,15 @@ type TestResult struct {
 	Value       interface{}        // Value is the object that was returned during the test
 	Changes     map[string]*Change // Changes is a slice of changes that were made during the test
 }
+
+func (t *TestResult) Pass(message string, value interface{}) {
+	t.Passed = true
+	t.Message = message
+	t.Value = value
+}
+
+func (t *TestResult) Fail(message string, value interface{}) {
+	t.Passed = false
+	t.Message = message
+	t.Value = value
+}
