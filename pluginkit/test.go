44716713626dab163d8445de46e756f309cd03e3
JM (Jason Meridth) <jmeridth@gmail.com>
2025-01-21 08:56:51 -0600

    chore: Update pluginkit/test.go
    Co-authored-by: Eddie Knight <knight@linux.com>
    Signed-off-by: JM (Jason Meridth) <jmeridth@gmail.com>

--- pluginkit/test.go
+++ pluginkit/test.go
@@ -7,7 +7,7 @@ type TestResult struct {
 	Passed      bool               `json:"passed"`      // Passed is true if the test passed
 	Description string             `json:"description"` // Description is a human-readable description of the test
 	Message     string             `json:"message"`     // Message is a human-readable description of the test result
-	Function    string             `json:"functin"`     // Function is the name of the code that was executed
+	Function    string             `json:"function"`     // Function is the name of the code that was executed
 	Value       interface{}        `json:"value"`       // Value is the object that was returned during the test
 	Changes     map[string]*Change `json:"changes"`     // Changes is a slice of changes that were made during the test
 }
