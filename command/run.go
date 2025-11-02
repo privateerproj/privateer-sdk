49fc669154e1af686973f0dde1b2d54253cb6618
Zohayb Bhatti <97380019+zohayb23@users.noreply.github.com>
2025-11-02 16:50:39 -0600

    feat: Add run command to SDK (#129)
    * feat: Add run command to SDK
    
    - Add Run() function to execute plugins with RPC handling
    - Add exit code constants (TestPass, TestFail, Aborted, etc.)
    - Add helper functions closeClient() and newClient()
    - Add PluginPkg types and GetPlugins() needed by run command
    - This enables sharing run functionality between OSS and Enterprise
    
    * test: add simple tests for config package functions
    
    Add simple input/output verification tests for unrecognized functions:
    - TestDefaultWritePath: verify path format and components
    - TestPrintSanitizedVars: verify function executes without panic
    - TestSetupLogging: verify logger is set correctly
    - TestSetupLoggingFilesAndDirectories: verify writer is returned
    
    These tests increase coverage as requested in PR #129
    
    * Update config/config_test.go
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * Update config/config_test.go
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    * linting
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    
    ---------
    
    Signed-off-by: Eddie Knight <knight@linux.com>
    Co-authored-by: Eddie Knight <knight@linux.com>

---
+++ command/run.go
@@ -0,0 +1,107 @@
+package command
+
+import (
+	"fmt"
+	"os/exec"
+
+	hclog "github.com/hashicorp/go-hclog"
+	hcplugin "github.com/hashicorp/go-plugin"
+	"github.com/spf13/viper"
+
+	"github.com/privateerproj/privateer-sdk/shared"
+)
+
+// Exit codes
+const (
+	TestPass = iota
+	TestFail
+	Aborted
+	InternalError
+	BadUsage
+	NoTests
+)
+
+// Run executes all plugins with handling for the command line
+func Run(logger hclog.Logger, getPlugins func() []*PluginPkg) (exitCode int) {
+	logger.Trace(fmt.Sprintf(
+		"Using bin: %s", viper.GetString("binaries-path")))
+
+	plugins := getPlugins()
+	if len(plugins) == 0 {
+		logger.Error(fmt.Sprintf("no plugins were requested in config: %s", viper.GetString("binaries-path")))
+		return NoTests
+	}
+
+	// Run all plugins
+	var runCount int
+	for serviceName := range viper.GetStringMap("services") {
+		servicePluginName := viper.GetString(fmt.Sprintf("services.%s.plugin", serviceName))
+		for _, pluginPkg := range plugins {
+			if pluginPkg.Name == servicePluginName {
+				if !pluginPkg.Available {
+					logger.Error(fmt.Sprintf("requested plugin that is not installed: %s", pluginPkg.Name))
+					return BadUsage
+				}
+				runCount++
+				client := newClient(pluginPkg.Command, logger)
+				// Connect via RPC
+				var rpcClient hcplugin.ClientProtocol
+				rpcClient, err := client.Client()
+				if err != nil {
+					logger.Error(fmt.Sprintf("internal error while initializing %s RPC client: %s", serviceName, err))
+					closeClient(pluginPkg, serviceName, client, logger)
+					return InternalError
+				}
+				// Request the plugin
+				var rawPlugin interface{}
+				rawPlugin, err = rpcClient.Dispense(shared.PluginName)
+				if err != nil {
+					logger.Error(fmt.Sprintf("internal error while dispensing RPC client: %s", err.Error()))
+					closeClient(pluginPkg, serviceName, client, logger)
+					return InternalError
+				}
+				// Execute plugin
+				plugin := rawPlugin.(shared.Pluginer)
+				logger.Trace(fmt.Sprintf("Starting Plugin %v: %s", runCount, pluginPkg.Name))
+				response := plugin.Start()
+				if response != nil {
+					pluginPkg.Error = fmt.Errorf("tests failed in plugin %s: %v", serviceName, response)
+					exitCode = TestFail
+				} else {
+					pluginPkg.Successful = true
+				}
+				closeClient(pluginPkg, serviceName, client, logger)
+			}
+		}
+	}
+	return exitCode
+}
+
+func closeClient(pluginPkg *PluginPkg, serviceName string, client *hcplugin.Client, logger hclog.Logger) {
+	// Close the client: this doesn't work via defer because it leaves the plugin running while the next begins
+	if pluginPkg.Successful {
+		logger.Info(fmt.Sprintf("Plugin for %s completed successfully", serviceName))
+	} else if pluginPkg.Error != nil {
+		logger.Error(pluginPkg.Error.Error())
+	} else {
+		logger.Error(fmt.Sprintf("unexpected exit while attempting to run package: %v", pluginPkg))
+	}
+	client.Kill()
+}
+
+// newClient client handles the lifecycle of a plugin application
+// Plugin hosts should use one Client for each plugin executable
+// (this is different from the client that manages gRPC)
+func newClient(cmd *exec.Cmd, logger hclog.Logger) *hcplugin.Client {
+	var pluginMap = map[string]hcplugin.Plugin{
+		shared.PluginName: &shared.Plugin{},
+	}
+	var handshakeConfig = shared.GetHandshakeConfig()
+	return hcplugin.NewClient(&hcplugin.ClientConfig{
+		HandshakeConfig: handshakeConfig,
+		Plugins:         pluginMap,
+		Cmd:             cmd,
+		Logger:          logger,
+	})
+}
+
